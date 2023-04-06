158f9e49595af0839df6d00277078dbe85a84772
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-04-06 17:03:10 -0300

    refactor: remove lib/ from git

--- lib/_plural.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns the plural of a string, which can be a single or a
- * compound word.
- */
-export default function _plural(string: string): string;
-//# sourceMappingURL=_plural.d.ts.map
--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns the plural form of the word.
- *
- * If quantity is given, it prefixes the output. The word is only
- * pluralized if |quantity| > 1.
- */
-export default function plural(word: string, quantity?: number | null): string;
-//# sourceMappingURL=index.d.ts.map
